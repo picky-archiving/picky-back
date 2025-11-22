df8862e075293b2631280e8c87ffe9fe578758b1
nayoung <dlskdud0311@naver.com>
2025-11-23 01:11:17 +0900

    fix: CommonErrorCode 수정

--- picky-back/src/main/java/com/example/pickyback/global/exception/CommonErrorCode.java
+++ picky-back/src/main/java/com/example/pickyback/global/exception/CommonErrorCode.java
@@ -1,29 +1,35 @@
-
 package com.example.pickyback.global.exception;
-
-import org.springframework.http.HttpStatus;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import org.springframework.http.HttpStatus;
 
 @Getter
 @AllArgsConstructor
 public enum CommonErrorCode implements ErrorCode {
 
-	// 서버 상태 (500)
-	INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "서버 상태가 원활하지 않습니다. 잠시 후 다시 시도해주세요."),
+    // 서버 상태 (500)
+    INTERNAL_SERVER_ERROR(
+            HttpStatus.INTERNAL_SERVER_ERROR,
+            "서버 상태가 원활하지 않습니다. 잠시 후 다시 시도해주세요."
+    ),
 
-	// 클라이언트가 요청 잘못했을 때 (400)
-	BAD_REQUEST(HttpStatus.BAD_REQUEST, "요청이 올바르지 않습니다. 입력 값을 다시 확인해주세요."),
+    // 클라이언트가 요청 잘못했을 때 (400)
+    BAD_REQUEST(
+            HttpStatus.BAD_REQUEST,
+            "요청이 올바르지 않습니다. 입력 값을 다시 확인해주세요."
+    ),
 
-    // 유저ID를 찾을 수 없을 때
-    USER_NOT_FOUND(HttpStatus.NOT_FOUND, "사용자를 찾을 수 없습니다."),
+    // 404 계열
+    USER_NOT_FOUND(
+            HttpStatus.NOT_FOUND,
+            "사용자를 찾을 수 없습니다."
+    ),
+    POLICY_NOT_FOUND(
+            HttpStatus.NOT_FOUND,
+            "정책이 존재하지 않습니다."
+    );   
 
-    // 정책ID를 찾을 수 없을 때
-    POLICY_NOT_FOUND(HttpStatus.NOT_FOUND, "존재하지 않는 정책입니다.");
-
-	POLICY_NOT_FOUNT_ERROR(HttpStatus.BAD_REQUEST, "정책이 존재하지 않습니다.");
-
-	private final HttpStatus status;
-	private final String message;
+    private final HttpStatus status;
+    private final String message;
 }

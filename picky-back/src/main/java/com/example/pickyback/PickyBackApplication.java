ccaa851838a0838cce251eeb51ba19276f76121c
nayoung <dlskdud0311@naver.com>
2025-11-22 15:22:48 +0900

    Add deploy.yml for CI/CD

---
+++ picky-back/src/main/java/com/example/pickyback/PickyBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.pickyback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PickyBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PickyBackApplication.class, args);
+	}
+
+}
